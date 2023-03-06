fcfd0407570e2350ef7bc868b7f5e40c20c8023f
irynaSokolovaUa <123591254+irynaSokolovaUa@users.noreply.github.com>
2023-03-06 14:08:03 +0200

    add class Slider

--- 013 Function Context/slider/script.js
+++ 013 Function Context/slider/script.js
@@ -3,6 +3,11 @@ class Slider{
         this.image = document.querySelector(selector + "img");
         this.btnNext = Document.querySelector(selector + "button")
         this.gallery = gallery;
+        this.i = 0;
+                this.btnNext.addEventListener('click', ()=>{
+                    this.i++;
+                    this.image.src = 'image/' + this.gallery[this.i];
+                })
     }
 
 }
