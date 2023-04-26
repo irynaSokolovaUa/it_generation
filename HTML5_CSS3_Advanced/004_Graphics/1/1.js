772842f03dc3044ffab1600fd3bdec750f3d90f3
irynaSokolovaUa <123591254+irynaSokolovaUa@users.noreply.github.com>
2023-04-26 14:57:44 +0300

    add finished task 2

---
+++ HTML5_CSS3_Advanced/004_Graphics/1/1.js
@@ -0,0 +1,13 @@
+const image = document.getElementById("img");
+
+document.getElementById("one").addEventListener("click", function() {
+  image.src = "img/Dog.jpeg";
+});
+
+document.getElementById("second").addEventListener("click", function() {
+  image.src = "img/turtle.jpeg";
+});
+
+document.getElementById("three").addEventListener("click", function() {
+  image.src = "img/2023-04-25 18.32.10.jpg";
+});
